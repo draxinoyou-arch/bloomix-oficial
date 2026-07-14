c827b0fefbe15ceb49a3bc2810187ce168a6eaf8
draxinoyou-arch <draxinoyou@gmail.com>
2026-07-13 20:30:15 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-// JavaScript de Bloomix Oficial
+
+let carrito = [];
+
+function agregarCarrito(nombre, precio) {
+  carrito.push({
+    nombre: nombre,
+    precio: precio
+  });
+
+  mostrarCarrito();
+}
+
+function mostrarCarrito() {
+  let html = "<h2>🛒 Carrito</h2>";
+
+  let total = 0;
+
+  carrito.forEach(producto => {
+    html += `<p>${producto.nombre} - S/ ${producto.precio}</p>`;
+    total += producto.precio;
+  });
+
+  html += `<h3>Total: S/ ${total}</h3>`;
+
+  document.getElementById("carrito").innerHTML = html;
+}
